21838e3a9cb4095c47b07e2ad01eb916c4cfd73e
FoxPlays <48643589+FoxPlays24@users.noreply.github.com>
2022-10-18 08:07:43 +0700

    тест

---
+++ index.js
@@ -0,0 +1,39 @@
+const tmi = require('tmi.js'),
+    { channel, username, password } = require('./settings.json');
+
+const options = {
+    options: { debug: true },
+    connection: {
+        recconect: true,
+        secure: true
+    },
+    identity: {
+        username,
+        password
+    },
+    channels: [channel]
+};
+
+const docChat = document.querySelector('#chat');
+const docCount = document.querySelector('#count');
+
+const client = new tmi.Client(options);
+client.connect().catch(console.error);
+
+/*client.on('connected', () => {
+    client.say(channel, 'BloodTrail');
+});*/
+
+let count = 0;
+
+client.on('message', (channel, tags, message, self) => {
+    if (self) return;
+    //console.log(`${tags['display-name']}: ${message}`);
+    docChat.textContent = `${tags['display-name']}: ${message}`;
+    docChat.style.color = `${tags.color}`;
+    if (message.includes('BloodTrail')) {
+        count++;
+        docCount.textContent = `Кол-во бладтрейлов за стрим: ${count}`;
+    }
+    console.log(tags.color);
+});
